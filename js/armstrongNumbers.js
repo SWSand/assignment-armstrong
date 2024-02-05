9db30e7e26aa30fab63ad69574cd1102aa136abb
Jose Sandoval <Software.Sandoval@gmail.com>
2024-02-05 15:08:12 -0600

    init

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,18 +1,22 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
-
-};
-
-
-function findArmstrongNumbers(arr){
-    armstrongArr = []
-
-    for(num in arr){
-        if (num <10){
-            armstrongArr.pop(num)
+exports.findArmstrongNumbers = function (arr) {
+    armstrongArr = [];
+    strArr = String(arr);
+  
+    for (num in arr) {
+      iExp = null;
+  
+      if (num < 10) {
+        armstrongArr.push(arr[num]);
+      } else {
+        for (i in String(arr[num])) {
+          iExp += (+String(arr[num])[i]) ** +String(arr[num]).length;
         }
+        if (iExp === +arr[num]) {
+          armstrongArr.push(arr[num]);
+        }
+      }
     }
-}
-
-console.log(findArmstrongNumbers([0,1,2,3,4,5,6,7,8,9,10,11,12]))
+    return armstrongArr;
+  }
